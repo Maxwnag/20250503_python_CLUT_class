cff5ce68a02cbffa3dd4be385a0de1da16b16563
arvin <eric860776@gmail.com>
2025-06-21 16:21:20 +0800

    1140621-9

--- lesson13/main.py
+++ lesson13/main.py
@@ -1,8 +1,7 @@
-import streamlit as st
+import numpy as np
+import pandas as pd
 
-def main():
-    st.title("我的第一個Streamlit App")
-    st.write("歡迎來到我的應用程式！")
-
-if __name__ == "__main__":
-    main()
+df = pd.read_csv('world.csv',index_col='國家',usecols=['國家','日期','總確診數','新增確診數','新增死亡數'])
+mask = df.index == "台灣"
+taiwan = df[mask]
+taiwan
